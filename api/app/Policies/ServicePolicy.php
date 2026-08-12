7dafe9603e9f629389510e8f093226c265dcc2a3
sardor-abdusattorov <s.abdusattorov@acdf.uz>
2026-08-12 09:44:05 +0500

    update

--- api/app/Policies/ServicePolicy.php
+++ api/app/Policies/ServicePolicy.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace App\Policies;
 
+use Illuminate\Foundation\Auth\User as AuthUser;
 use App\Models\Service;
 use Illuminate\Auth\Access\HandlesAuthorization;
-use Illuminate\Foundation\Auth\User as AuthUser;
 
 class ServicePolicy
 {
     use HandlesAuthorization;
-
+    
     public function viewAny(AuthUser $authUser): bool
     {
         return $authUser->can('ViewAny:Service');
@@ -71,4 +71,5 @@ class ServicePolicy
     {
         return $authUser->can('Reorder:Service');
     }
+
 }
