ba3fd3304ea38762b5e3bc8f66e35f5f1bcd1c3d
sardor-abdusattorov <s.abdusattorov@fund.uz>
2026-08-14 14:08:14 +0500

    update

--- api/app/Policies/ApplicationThemePolicy.php
+++ api/app/Policies/ApplicationThemePolicy.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace App\Policies;
 
+use Illuminate\Foundation\Auth\User as AuthUser;
 use App\Models\ApplicationTheme;
 use Illuminate\Auth\Access\HandlesAuthorization;
-use Illuminate\Foundation\Auth\User as AuthUser;
 
 class ApplicationThemePolicy
 {
     use HandlesAuthorization;
-
+    
     public function viewAny(AuthUser $authUser): bool
     {
         return $authUser->can('ViewAny:ApplicationTheme');
@@ -71,4 +71,5 @@ class ApplicationThemePolicy
     {
         return $authUser->can('Reorder:ApplicationTheme');
     }
+
 }
